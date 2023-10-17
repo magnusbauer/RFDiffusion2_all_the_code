Fix metrics issubclass and r3 scaling bug.  Add bond length metrics.

rf_diffusion/bond_geometry.py
@@ -7,8 +7,9 @@ import numpy as np
 from rf2aa import chemical
 from typing import Iterable
 from rf_diffusion.graph_utils import ListDict, find_subgraphs
+from rf_diffusion import aa_model
 
-def calc_atom_bond_loss(indep, pred_xyz, is_diffused):
+def calc_atom_bond_loss(indep, pred_xyz, is_diffused, point_types):
     """
     Loss on distances between bonded atoms
     """
@@ -18,16 +19,19 @@ def calc_atom_bond_loss(indep, pred_xyz, is_diffused):
     # is_ligand = indep.is_sm & ~(indep.seq < rf2aa.chemical.NPROTAAS)
     mask_by_name = {}
     for k, v in {
-        'residue': ~indep.is_sm,
-        'atom': indep.is_sm,
+        'residue': point_types == aa_model.POINT_RESIDUE,
+        'atomized_sidechain': point_types == aa_model.POINT_ATOMIZED_SIDECHAIN,
+        'atomized_backbone': point_types == aa_model.POINT_ATOMIZED_BACKBONE,
+        'atomized': np.isin(point_types, [aa_model.POINT_ATOMIZED_BACKBONE, aa_model.POINT_ATOMIZED_SIDECHAIN]),
+        'ligand': point_types == aa_model.POINT_LIGAND,
+        'any': np.full(indep.length(), True),
     }.items():
         for prefix, mask in {
             'diffused': is_diffused,
-            'motif': ~is_diffused
+            'motif': ~is_diffused,
+            'any': np.full(indep.length(), True),
         }.items():
-            mask_by_name[f'{prefix}_{k}'] = v*mask
-    mask_by_name['all'] = torch.ones_like(is_diffused).bool()
-
+            mask_by_name[f'{prefix}_{k}'] = torch.tensor(v)*mask
     bond_losses = {}
     true_xyz = indep.xyz
     is_bonded = torch.triu(indep.bond_feats > 0)

rf_diffusion/metrics.py
@@ -1,4 +1,6 @@
+import inspect
 import torch
+import numpy as np
 from icecream import ic
 import itertools
 from rf_diffusion import bond_geometry
@@ -94,8 +96,8 @@ def contigs(logit_s, label_s,
         'n_contig_res': n_contig_res(diffusion_mask),
     }
 
-def atom_bonds(indep, pred_crds, is_diffused, **kwargs):
-    return bond_geometry.calc_atom_bond_loss(indep, pred_crds, is_diffused)
+def atom_bonds(indep, pred_crds, is_diffused, point_types, **kwargs):
+    return bond_geometry.calc_atom_bond_loss(indep, pred_crds, is_diffused, point_types)
 
 ###################################
 # Bond geometry metrics
@@ -177,12 +179,13 @@ class VarianceNormalizedTransMSE():
         other_crds: torch.Tensor, 
         true_crds: torch.Tensor, 
         t: float, 
-        is_diffused: torch.Tensor
+        is_diffused: torch.Tensor,
+        **kwargs
         ):
 
         # Raw mean squared error over diffused atoms
-        true_crds = true_crds[..., is_diffused, 1, :]
-        other_crds = other_crds[..., is_diffused, 1, :]
+        true_crds = true_crds[..., is_diffused, 1, :] * self.r3_diffuser._r3_conf.coordinate_scaling
+        other_crds = other_crds[..., is_diffused, 1, :] * self.r3_diffuser._r3_conf.coordinate_scaling
         mse = loss.mse(other_crds, true_crds)
 
         # Normalize MSE by the variance of the added noise
@@ -203,9 +206,9 @@ class VarianceNormalizedPredTransMSE(Metric):
         true_crds: torch.Tensor, 
         input_crds: torch.Tensor, 
         t: float, 
-        is_diffused: torch.Tensor
+        is_diffused: torch.Tensor,
+        **kwargs
         ):
-
         return self.get_variance_normalized_mse(pred_crds, true_crds, t, is_diffused)
 
 class VarianceNormalizedInputTransMSE(Metric):
@@ -219,7 +222,8 @@ class VarianceNormalizedInputTransMSE(Metric):
         true_crds: torch.Tensor, 
         input_crds: torch.Tensor, 
         t: float, 
-        is_diffused: torch.Tensor
+        is_diffused: torch.Tensor,
+        **kwargs,
         ):
 
         return self.get_variance_normalized_mse(input_crds, true_crds, t, is_diffused)
@@ -243,7 +247,7 @@ class MetricManager:
             obj = getattr(thismodule, name)
             # Currently support metrics being Metric subclass or a ducktyped function with identical call signature.
             # Might change to only supporting Metric subclasses in the future.
-            if issubclass(obj, Metric):
+            if inspect.isclass(obj) and issubclass(obj, Metric):
                 self.metric_callables[name] = obj(conf)
             elif callable(obj):
                 self.metric_callables[name] = obj
@@ -258,7 +262,8 @@ class MetricManager:
         true_crds: torch.Tensor, 
         input_crds: torch.Tensor, 
         t: float, 
-        is_diffused: torch.Tensor
+        is_diffused: torch.Tensor,
+        point_types: np.array
         ) -> dict:
         '''
         Inputs
@@ -290,6 +295,7 @@ class MetricManager:
                 input_crds=input_crds,
                 t=t,
                 is_diffused=is_diffused,
+                point_types=point_types
             )
             metric_results[name] = metric_output
 

rf_diffusion/train_multi_deep.py
@@ -882,12 +882,16 @@ class Trainer():
                 #    for n, p in ddp_model.named_parameters():
                 #        if p.grad is None and p.requires_grad is True:
                 #            print('Parameter not used:', n, p.shape)  # prints unused parameters. Remove them from your model
-                
-                atom_ids = np.arange(L)
-                if atomizer:
-                    atom_ids = atomize.get_res_atom_name_by_atomized_idx(atomizer)
-                ic(atom_ids, L)
 
+                metrics_inputs = dict(
+                    indep=indep,
+                    pred_crds=pred_crds[-1, 0].cpu(),
+                    true_crds=true_crds[0, :, :3].cpu(),
+                    input_crds=xyz_prev_orig[:, :3].cpu(),
+                    t=little_t/self.diffuser.T,
+                    is_diffused=is_diffused.cpu(),
+                    point_types=aa_model.get_point_types(indep, atomizer)
+                )
                 log_metrics = (counter % N_PRINT_TRAIN == 0) and (rank == 0)
                 if log_metrics:
                     train_time  = time.time() - start_time
@@ -927,14 +931,6 @@ class Trainer():
                             'loss':loss.detach()})
 
                         rf2aa.tensor_util.to_device(indep, 'cpu')
-                        metrics_inputs = dict(
-                            indep=indep,
-                            pred_crds=pred_crds[-1, 0].cpu(),
-                            true_crds=true_crds[0, :, :3].cpu(),
-                            input_crds=xyz_prev_orig[:, :3].cpu(),
-                            t=little_t/self.diffuser.T,
-                            is_diffused=is_diffused.cpu(),
-                        )
                         metrics = self.metric_manager.compute_all_metrics(**metrics_inputs)
                                     
                         loss_dict['metrics'] = metrics