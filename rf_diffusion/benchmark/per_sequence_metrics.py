e8bd697f8ec55b81de750fb299f2e0a938d81e44
Doug Tischer <dtischer@uw.edu>
2024-01-28 22:44:23 -0800

    Trivial.

--- rf_diffusion/benchmark/per_sequence_metrics.py
+++ rf_diffusion/benchmark/per_sequence_metrics.py
@@ -126,7 +126,6 @@ def catalytic_constraints_inner(pdb, mpnn_packed: bool):
         # f['name'] = utils.process_target(pdb, parse_hetatom=True, center=False)
         indeps[name] = aa_model.make_indep(pdb, ligand=None if name == 'af2' else ligand)
         is_atomized = ~indeps[name].is_sm
-        # indeps_a[name], atomizers[name] = atomize.atomize(indeps[name], is_atomized)
         atomization_state = aa_model.get_atomization_state(indeps[name])
         atomizers[name] = aa_model.AtomizeResidues(atomization_state, is_atomized)
         indeps_a[name] = atomizers[name].atomize(indeps[name])
