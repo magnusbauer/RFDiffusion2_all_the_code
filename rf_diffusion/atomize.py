6c0218e765769f7e1d82b4f0ea4604e62dec478f
Magnus Bauer <magnusb@uw.edu>
2024-05-16 11:48:36 -0700

    Adding empty entry to trb['motif'] if contig_atoms atom id is empty (#236)
    Changing `atomize.convert_atomized_mask` to check if a residue was
    atomized but no motif atom was selected in the contig_atoms. In this
    case an empty entry is added to the motif for this residue so
    show_bench.py can correctly visualize an atomized residue without motif
    selected.

--- rf_diffusion/atomize.py
+++ rf_diffusion/atomize.py
@@ -111,6 +111,14 @@ def convert_atomized_mask(atomizer, mask):
             o[deatomized_i].append('ALL')
         else:
             raise Exception(f'{atomized_i} not found')
+        
+    atm_idx_list = [t[0] for t in res_idx_atom_name_by_atomized_idx.values()]
+    unique_atm_idx_list = list(set(atm_idx_list))
+
+    for atm_idx in unique_atm_idx_list:
+        if atm_idx not in list(o.keys()):
+            o[atm_idx].append('')
+
     return o
 
 def atomized_indices_from_preatomized_res_indices(atomizer, res_indices):
